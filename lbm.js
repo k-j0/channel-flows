abac34b0dd31617c77a3dc46aecb2d44ee8ad1e2
Jon <jonathan-kings@hotmail.com>
2021-10-18 19:13:07 +0200

    Implement constant pressure gradient sim

--- lbm.js
+++ lbm.js
@@ -5,27 +5,32 @@
 class D2Q9 {
 
     /// Simulation constants (always the same across any D2Q9 sim)
+    // Order of i is chosen clockwise, to make full bounce-back conditions easy to compute with modulos
+    // 6 7 8
+    // 5 0 1
+    // 4 3 2
+    // Hence the weights and velocities look 'out of order' compared to typical order with 1 <= i <= 4 on the sides and 5 <= i <= 8 on the corners
     static W = [ // weights
-        4.0 / 9.0,  // i = 0
-        1.0 / 9.0,  // i = 1
-        1.0 / 9.0,  // i = 2
-        1.0 / 9.0,  // i = 3
-        1.0 / 9.0,  // i = 4
-        1.0 / 36.0, // i = 5
-        1.0 / 36.0, // i = 6
-        1.0 / 36.0, // i = 7
-        1.0 / 36.0  // i = 8
+        4.0 / 9.0,  // i = 0 (centre)
+        1.0 / 9.0,  // i = 1 (side)
+        1.0 / 36.0, // i = 2 (corner)
+        1.0 / 9.0,  // i = 3 (side)
+        1.0 / 36.0, // i = 4 (corner)
+        1.0 / 9.0,  // i = 5 (side)
+        1.0 / 36.0, // i = 6 (corner)
+        1.0 / 9.0,  // i = 7 (side)
+        1.0 / 36.0  // i = 8 (corner)
     ];
     static C = [ // velocities
         new THREE.Vector2(0, 0),    // i = 0
         new THREE.Vector2(1, 0),    // i = 1
-        new THREE.Vector2(0, -1),   // i = 2
-        new THREE.Vector2(-1, 0),   // i = 3
-        new THREE.Vector2(0, 1),    // i = 4
-        new THREE.Vector2(1, -1),   // i = 5
+        new THREE.Vector2(1, 1),    // i = 2
+        new THREE.Vector2(0, 1),    // i = 3
+        new THREE.Vector2(-1, 1),   // i = 4
+        new THREE.Vector2(-1, 0),   // i = 5
         new THREE.Vector2(-1, -1),  // i = 6
-        new THREE.Vector2(-1, 1),   // i = 7
-        new THREE.Vector2(1, 1)     // i = 8
+        new THREE.Vector2(0, -1),   // i = 7
+        new THREE.Vector2(1, -1)    // i = 8
     ];
     
 
@@ -37,9 +42,20 @@ class D2Q9 {
      * @param {THREE.Vector2} velocity The velocity u
      */
     static NiEq (i, density, velocity) {
-        let uDotU = velocity.lengthSq(); // u • u
+        let uDotU = velocity.dot(velocity); // u • u
         let ciDotU = D2Q9.C[i].dot(velocity); // c_i • u
         return D2Q9.W[i] * density * (1.0 + 3.0 * ciDotU + 4.5 * ciDotU * ciDotU - 1.5 * uDotU);
+    }
+
+    /**
+     * Computes the opposite direction from direction i
+     * 
+     * @param {number} i Direction for which to get the opposing dir
+     * @returns {number} Opposite direction to input
+     */
+    static opposite (i) {
+        if (i == 0) return 0;
+        return ((i - 1 + 4) % 8) + 1;
     }
 
 
@@ -61,6 +77,9 @@ class D2Q9 {
      */
     static Site = class {
 
+        // whether the site is solid (boundary site); particles that end up on solid sites will bounce back
+        boundary = false;
+
         // distributions Ni for 0 <= i < 9
         n = [
             0,  // i = 0
@@ -72,6 +91,19 @@ class D2Q9 {
             0,  // i = 6
             0,  // i = 7
             0   // i = 8
+        ];
+
+        // temp distributions Ni after collision step
+        nt = [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
         ];
 
         // local density (sum over Ni)
@@ -95,7 +127,7 @@ class D2Q9 {
 
         /**
          * Collision step; updates density and velocity based on Ni values
-         * Updates ρ and u then Ni vals
+         * Updates ρ and u then Ni vals (nt)
          * 
          * @param {float} omega The relaxation time inverse omega used for the simulation
          * @param {THREE.Vector2} force Forcing to add to the particles
@@ -107,9 +139,10 @@ class D2Q9 {
             this.velocity.set(0, 0);
             for(let i = 0; i < 9; ++i) {
                 this.density += this.n[i];
-                this.velocity.addScaledVector(D2Q9.C[i], this.n[i]);
+                this.velocity.addScaledVector(D2Q9.C[i], this.n[i]); // ρu = sum_i( c_i * N_i )
             }
-            this.velocity.multiplyScalar(1.0 / this.density); // rho * u = sum_i( c_i N_i )
+            this.velocity.x += 1e-2; // horizontal pressure gradient; TODO remove this
+            this.velocity.multiplyScalar(1.0 / this.density); // ρu = sum_i( c_i * N_i ) hence u = sum_i(c_i * N_i ) / ρ
 
             // collision step for the site
             let F = force.clone();
@@ -118,10 +151,12 @@ class D2Q9 {
                 let Nieq = D2Q9.NiEq(i, this.density, this.velocity);
                 // Ni = Ni - omega * (Ni - Ni^eq)
                 // i.e. Ni = lerp(Ni, Ni^eq, omega)
-                this.n[i] = (1.0 - omega) * this.n[i] + omega * Nieq;
-                // add forcing term
-                // 3 Wi Ci • ρF
-                this.n[i] += 3.0 * D2Q9.W[i] * D2Q9.C[i].dot(F);
+                this.nt[i] = (1.0 - omega) * this.n[i] + omega * Nieq;
+                if (this.nt[i] < 0) {
+                    this.nt[i] = 0;
+                }
+                // add forcing term   3 Wi Ci • F   with   F = ρf
+                //this.n[i] += 3.0 * D2Q9.W[i] * D2Q9.C[i].dot(F); // <- TODO add this back in!
             }
         }
 
@@ -138,9 +173,12 @@ class D2Q9 {
     force; // THREE.Vector2
 
     /// Array of individual lattice sites
-    /// Front and back buffer, with ability to swap which is being read from and which is being written to
-    sites; // [ D2Q9.Site[], D2Q9.Site[] ]
-    flipBuffers () { this.sites = [ this.sites[1], this.sites[0] ]; } // flip front and back site buffer
+    sites; // D2Q9.Site[]
+    
+    /// Time-dependent values (not needed for simulation, just for display purposes)
+    t = 0; // time, i.e. number of steps taken
+    density = 0; // average density over the lattice
+    velocity = new THREE.Vector2(0, 0); // average velocity over the lattice
 
 
     
@@ -155,32 +193,53 @@ class D2Q9 {
         this.omega = 1.0 / (3.0 * params.kinematicViscosity + 0.5); // to fulfill Navier-Stokes eq requirements (ν = ⅓(ω⁻¹ - ½))
         this.force = params.force;
 
-        // init sites - front and back buffer
-        this.sites = [[], []];
+        // init sites
+        this.sites = [];
         for(let x = 0; x < this.width; ++x) {
             for(let y = 0; y < this.height; ++y) {
-                let frontSite = new D2Q9.Site(params.initialDensity);
-                let backSite = new D2Q9.Site(0);
-                this.sites[1].push(backSite); // front buffer for the first simulation step
-                this.sites[0].push(frontSite); // back buffer - uninitialized for now
+                this.sites.push(new D2Q9.Site(params.initialDensity));
             }
         }
 
     }
 
     /**
-     * Site getter, for easier access than 1d array index, accessing either a site from the front or back buffer
+     * Adds a boundary on a single site
+     * 
+     * @param {int} x X-coordinate of the site to make into a boundary site
+     * @param {int} y Y-coordinate of the site to make into a boundary site
+     */
+    addBoundary (x, y) {
+        this.getSite(x, y).boundary = true;
+        for (let i = 0; i < 9; ++i) {
+            this.getSite(x, y).n[i] = 0;
+        }
+    }
+
+    /**
+     * Adds a horizontal bounce-back boundary for sites at y
+     * 
+     * @param {int} y Y-coordinate of sites to make into boundaries
+     */
+    addHorizontalBoundary(y) {
+        for(let x = 0; x < this.width; ++x) {
+            this.addBoundary(x, y);
+        }
+    }
+
+    /**
+     * Site getter, for easier access than 1d array index
+     * Handles periodicity on both axes
      * 
      * @param {number} x Index of the site to obtain on the x axis
      * @param {number} y Index of the site to obtain on the y axis
-     * @param {bool} read Whether to access the site from the front or back buffer (read = front, write = back)
      */
-    getSite (x, y, read) {
+    getSite (x, y) {
         while(x < 0) x += this.width; // handle negative indices
         while(y < 0) y += this.height;
         x = x % this.width; // remap to 0..width
         y = y % this.height; // remap to 0..height
-        return this.sites[read ? 0 : 1][x * this.height + y];
+        return this.sites[x * this.height + y];
     }
 
     /**
@@ -188,36 +247,78 @@ class D2Q9 {
      */
     step () {
 
-        // Propagation step
-        for(let x = 0; x < this.width; ++x) {
-            for(let y = 0; y < this.height; ++y) {
+        this.density = 0;
+        this.velocity.set(0, 0);
 
-                // propagate from each of the 9 direction from neighbouring cells
-                let site = this.getSite(x, y, false);
-                for(let i = 0; i < 9; ++i) {
-                    let c = D2Q9.C[i];
-                    let dx = -parseInt(c.x);
-                    let dy = -parseInt(c.y);
-                    site.n[i] = this.getSite(x + dx, y + dy, true).n[i];
+        // Collision step
+        for (let x = 0; x < this.width; ++x) {
+            for (let y = 0; y < this.height; ++y) {
+
+                // get site to update
+                let site = this.getSite(x, y);
+                
+                // skip calculations for obstacle sites
+                if (site.boundary) continue;
+
+                // collision step for the site
+                site.collision(this.omega, this.force);
+            }
+        }
+
+        // Propagation step
+        for (let x = 0; x < this.width; ++x) {
+            for (let y = 0; y < this.height; ++y) {
+
+                // get site to update
+                let site = this.getSite(x, y);
+
+                // skip boundary sites
+                if (site.boundary) continue;
+
+                // stream particle distribs from neighbours
+                for (let i = 0; i < 9; ++i) {
+                    let dx = D2Q9.C[i].x;
+                    let dy = D2Q9.C[i].y;
+                    site.n[i] = this.getSite(x + dx, y + dy).nt[i];
                 }
 
             }
         }
 
-        // Bounce-back for no-slip
-        // @todo
+        // Bounce-back boundary conditions
+        for (let x = 0; x < this.width; ++x) {
+            for (let y = 0; y < this.height; ++y) {
 
-        // Swap front and back buffers
-        this.flipBuffers();
+                // get site to push particles away from
+                let site = this.getSite(x, y);
+                
+                // skip fluid sites
+                if (!site.boundary) continue;
 
-        // Collision step
-        for(let x = 0; x < this.width; ++x) {
-            for(let y = 0; y < this.height; ++y) {
+                // half-way bounce-back
+                for (let i = 0; i < 9; ++i) {
+                    let dx = -D2Q9.C[i].x;
+                    let dy = -D2Q9.C[i].y;
+                    site.n[i] = this.getSite(x + dx, y + dy).n[D2Q9.opposite(i)];
+                }
+            }
+        }
 
-                this.getSite(x, y, true).collision(this.omega, this.force);
+        // Compute avg density and velocity
+        for (let x = 0; x < this.width; ++x) {
+            for (let y = 0; y < this.height; ++y) {
+
+                let site = this.getSite(x, y);
+                this.density += site.density;
+                this.velocity.add(site.velocity);
 
             }
         }
+        this.density /= this.width * this.height;
+        this.velocity.multiplyScalar(1.0 / (this.width * this.height));
+
+        // t -> t+1
+        ++this.t;
 
     }
 
