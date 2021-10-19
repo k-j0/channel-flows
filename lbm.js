a3cbc7272bf858416dcbdbed21e6f7eb25129754
Jon <jonathan-kings@hotmail.com>
2021-10-19 19:37:20 +0200

    Implement occupation plot

--- lbm.js
+++ lbm.js
@@ -360,4 +360,33 @@ class D2Q9 {
 
     }
 
+    /**
+     * Plots the average values of N_i across the entire lattice
+     * 
+     * @param {string} colour Colour to use on the chart
+     */
+    plotOccupations (colour = 'red') {
+        
+        let labels = [];
+        let data = [];
+
+        for (let i = 0; i < 9; ++i) {
+            // average through lattice
+            let avg = 0;
+            for (let x = 0; x < this.width; ++x) {
+                for (let y = 0; y < this.height; ++y) {
+                    let site = this.getSite(x, y);
+                    if (!site.boundary) {
+                        avg += site.n[i];
+                    }
+                }
+            }
+            avg /= this.width * this.height * D2Q9.W[i];
+            labels.push('i = ' + i);
+            data.push(avg);
+        }
+
+        plot(labels, data, 'Occupation average across lattice at t = ' + this.t, colour);
+    }
+
 }; // class D2Q9
